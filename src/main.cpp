c491ba6c8f55abc4826db076c9a742814c99784a
Felipe Bombardelli <felipebombardelli@gmail.com>
2016-06-03 16:35:03 -0300

    Added fake to video

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,8 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <tiobj.hpp>
 #include <unistd.h>
-
+#include <tisys.hpp>
+#include <stdio.h>
 
 using namespace std;
 using namespace cv;
@@ -14,16 +15,40 @@ using namespace cv;
 
 int main(){
 	TiObj params( getenv("params") );
-	int freq = params.atInt("freq");
+	int freq   = params.atInt("freq",10);
+	string robot_url = getenv("robot_url");
+	string url = robot_url + "/" + params.atStr("url");
 	unsigned sleep_time = 1000000 / freq;
+	
 
+	TiObj file;
+	Filesystem fs;
+	fs.info(url);
+	cerr << "aaa" << fs;
 	Mat img;
-	while ( true ){
-		img.create(480,640,CV_8UC3);
-		img.setTo(0);
-		imwrite("image.jpg",img);
-		usleep(sleep_time);
+
+	if ( file.is("Image") ){
+		img = imread(url);
+		while ( true ){
+			imwrite("image.jpg",img);
+			usleep(sleep_time);
+			printf("#end");
+			fflush(stdout);
+		}
+
+	} else if ( file.is("Video") ){
+		VideoCapture video;
+		video.open(url);
+		while ( true ){
+			video >> img;
+			imwrite("image.jpg",img);
+			usleep(sleep_time);
+			printf("#end");
+			fflush(stdout);
+		}
+
 	}
+
 
 	return 0;
 }
